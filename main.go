d2a555516eeb542e3e5af49d282a4d831172903c
Ilya Stone <tnt228337@gmail.com>
2023-09-05 09:00:17 +0300

    write func save_list in main.go

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var (
+	configFile string
+	SiteList   map[string]int
+)
+
+func init() {
+	SiteList = make(map[string]int)
+	flag.StringVar(&configFile, "config", "config.json", "config file")
+
+}
+
+func save_list() {
+	data, err := json.Marshal(SiteList)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(configFile, data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
 }
