e02223727ff1415ee3a346f669dbdab97b8ee16c
Ilya Stone <tnt228337@gmail.com>
2023-09-06 17:34:12 +0300

    create func send_notif in main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -10,13 +11,40 @@ import (
 )
 
 var (
-	configFile string
-	SiteList   map[string]int
+	SiteList              map[string]int
+	chatID                int64
+	sslDaysToExipireAlert int64
+	telegramBotToken      string
+	configFile            string
+	pprofListen           string
+	HelpMsg               = "Это простой мониторинг доступности сайтов. Он обходит сайты в списке и ждет что он ответит 200, если возвращается не 200 или ошибки подключения, то бот пришлет уведомления в групповой чат\n" +
+		"Список доступных комманд:\n" +
+		"/site_list - покажет список сайтов в мониторинге и их статусы (про статусы ниже)\n" +
+		"/site_add [url] - добавит url в список мониторинга\n" +
+		"/site_del [url] - удалит url из списка мониторинга\n" +
+		"/help - отобразить это сообщение\n" +
+		"\n" +
+		"У сайтов может быть несколько статусов:\n" +
+		"0 - никогда не проверялся (ждем проверки)\n" +
+		"1 - ошибка подключения \n" +
+		"2 - истекает сертификат \n" +
+		"200 - ОК-статус" +
+		"все остальные http-коды считаются некорректными"
 )
 
 func init() {
 	SiteList = make(map[string]int)
 	flag.StringVar(&configFile, "config", "config.json", "config file")
+
+}
+
+func send_notif(bot *tgbotapi.BotAPI) {
+	for site, status := range SiteList {
+		if status != 200 {
+			alarm := fmt.Sprintf("CRIT - %s ; status: %v", site, status)
+			bot.Send(tgbotapi.NewMessage(chatID, alarm))
+		}
+	}
 
 }
 
@@ -44,7 +72,7 @@ func load_list() {
 		return
 	}
 
-	log.Printf(string(data))
+	log.Println(string(data))
 }
 
 func main() {
