2a4a36bb700bcca9dd3917b6ee381b54b7c79cb4
Ilya Stone <tnt228337@gmail.com>
2023-09-07 07:05:30 +0300

    write monitor func in main.go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -35,6 +39,18 @@ var (
 func init() {
 	SiteList = make(map[string]int)
 	flag.StringVar(&configFile, "config", "config.json", "config file")
+
+	if telegramBotToken == "" {
+		log.Print("-telegrambottoken is required")
+		os.Exit(1)
+	}
+
+	if chatID == 0 {
+		log.Print("-chatid is required")
+		os.Exit(1)
+	}
+
+	load_list()
 
 }
 
@@ -75,9 +91,64 @@ func load_list() {
 	log.Println(string(data))
 }
 
+func monitor(bot *tgbotapi.BotAPI) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	var httpClient = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: tr,
+	}
+
+	for {
+		save_list()
+
+		for site, _ := range SiteList {
+			response, err := httpClient.Get(site)
+			if err != nil {
+				SiteList[site] = 1
+				log.Printf("Status of %s: %s: %s", site, "1 - Connection error", err)
+			} else {
+				log.Printf("Status of %s: %s", site, response.Status)
+				SiteList[site] = response.StatusCode
+
+				siteUrl, err := url.Parse(site)
+				if err != nil {
+					panic(err)
+				}
+
+				if siteUrl.Scheme == "https" {
+					conn, err := tls.Dial("tcp", siteUrl.Host+":443", tr.TLSClientConfig)
+					if err != nil {
+						log.Printf("Error in SSL dial to %s: %s", siteUrl.Host, err)
+					}
+
+					certs := conn.ConnectionState().PeerCertificates
+
+					for _, cert := range certs {
+						difference := time.Since(cert.NotAfter)
+						dayToExspire := int64(difference.Hours() / 24)
+						if dayToExspire > -(sslDaysToExipireAlert) {
+							log.Printf("Status of %s: %s", site, "2 - certificate is expiring")
+							SiteList[site] = 2
+						}
+					}
+
+					conn.Close()
+				}
+			}
+		}
+
+		send_notif(bot)
+		time.Sleep(time.Minute * 5)
+	}
+}
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
+
 }
